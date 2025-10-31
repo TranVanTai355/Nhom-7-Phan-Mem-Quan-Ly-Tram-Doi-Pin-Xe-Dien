7a393c2b69cf10c82c4f5605764c909d5599e1b0
thanhnvd1101 <thanhnvd1101@ut.edu.vn>
2025-10-31 19:03:24 +0700

    Add getter and setter methods for TaiXe fields

--- src/main/java/com/nhom7/quanlytrampin/entity/PhuongTien.java
+++ src/main/java/com/nhom7/quanlytrampin/entity/PhuongTien.java
@@ -20,4 +20,52 @@ public class PhuongTien {
     @ManyToOne
     @JoinColumn(name = "MaTaiXe", nullable = false) 
     private TaiXe taiXe;
+
+        public Long getMaTaiXe() {
+        return maTaiXe;
+    }
+
+    public void setMaTaiXe(Long maTaiXe) {
+        this.maTaiXe = maTaiXe;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public List<PhuongTien> getDanhSachPhuongTien() {
+        return danhSachPhuongTien;
+    }
+
+    public void setDanhSachPhuongTien(List<PhuongTien> danhSachPhuongTien) {
+        this.danhSachPhuongTien = danhSachPhuongTien;
+    }
 }
