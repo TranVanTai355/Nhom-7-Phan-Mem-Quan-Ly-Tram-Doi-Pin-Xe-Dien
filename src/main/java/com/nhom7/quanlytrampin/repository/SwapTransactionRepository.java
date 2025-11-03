9fd65f70c4ace19f13ca4edb9c75323f23f4f32e
N7oan <86726151+N7oan@users.noreply.github.com>
2025-11-03 17:27:43 +0700

    Create SwapTransactionRepository.java

---
+++ src/main/java/com/nhom7/quanlytrampin/repository/SwapTransactionRepository.java
@@ -0,0 +1,7 @@
+package com.nhom7.quanlytrampin.repository;
+
+import com.nhom7.quanlytrampin.entity.SwapTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwapTransactionRepository extends JpaRepository<SwapTransaction, Long> {
+}
