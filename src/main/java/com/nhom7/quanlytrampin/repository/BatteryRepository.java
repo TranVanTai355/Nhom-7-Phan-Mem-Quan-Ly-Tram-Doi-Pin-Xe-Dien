f6e20710604b2976c620bfbc55cdef7785c0ee3d
N7oan <86726151+N7oan@users.noreply.github.com>
2025-11-03 17:21:12 +0700

    Update BatteryRepository.java

--- src/main/java/com/nhom7/quanlytrampin/repository/BatteryRepository.java
+++ src/main/java/com/nhom7/quanlytrampin/repository/BatteryRepository.java
@@ -1,1 +1,11 @@
+package com.nhom7.quanlytrampin.repository;
+
+import com.nhom7.quanlytrampin.entity.Battery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BatteryRepository extends JpaRepository<Battery, Long> {
+
+}
 
