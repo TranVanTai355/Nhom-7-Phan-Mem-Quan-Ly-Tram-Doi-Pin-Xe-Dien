bb5f839e9c484a82b3c6211f5eebd1952ae9b9e6
thanhnvd1101 <thanhnvd1101@ut.edu.vn>
2025-11-08 21:52:17 +0700

    Update SwapTransaction.java

--- src/main/java/com/nhom7/quanlytrampin/entity/SwapTransaction.java
+++ src/main/java/com/nhom7/quanlytrampin/entity/SwapTransaction.java
@@ -30,4 +30,61 @@ public class SwapTransaction {
     private String returnedBatteryCondition;
 
     private LocalDateTime createdAt = LocalDateTime.now();
+
+        // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Battery getOldBattery() {
+        return oldBattery;
+    }
+    public void setOldBattery(Battery oldBattery) {
+        this.oldBattery = oldBattery;
+    }
+
+    public Battery getNewBattery() {
+        return newBattery;
+    }
+    public void setNewBattery(Battery newBattery) {
+        this.newBattery = newBattery;
+    }
+
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+    public void setPaymentAmount(double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReturnedBatteryCondition() {
+        return returnedBatteryCondition;
+    }
+    public void setReturnedBatteryCondition(String returnedBatteryCondition) {
+        this.returnedBatteryCondition = returnedBatteryCondition;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
