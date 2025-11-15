37cfd88303a93512cf6b213016fe94b8d6f1266a
minhduc2207 <taminhduc22072005@gmail.com>
2025-11-16 05:40:08 +0700

    Update AuthController.java

--- src/main/java/com/nhom7/quanlytrampin/controller/AuthController.java
+++ src/main/java/com/nhom7/quanlytrampin/controller/AuthController.java
@@ -1,33 +1,41 @@
 package com.nhom7.quanlytrampin.controller;
 
-import com.nhom7.quanlytrampin.entity.TaiXe;
 import com.nhom7.quanlytrampin.dto.LoginRequestDTO;
+import com.nhom7.quanlytrampin.entity.TaiXe;
 import com.nhom7.quanlytrampin.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-@RestController 
-@RequestMapping("/api/auth") 
+import java.util.Map;
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
-    
     @PostMapping("/register")
     public ResponseEntity<?> registerDriver(@RequestBody TaiXe taiXe) {
         try {
             TaiXe newDriver = authService.registerDriver(taiXe);
-            return ResponseEntity.ok(newDriver); 
+            return ResponseEntity.ok(newDriver);
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage()); 
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
-@PostMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
- 
-        return ResponseEntity.ok("Login API đã được gọi!");
+        try {
+            String token = authService.loginUser(
+                    loginRequest.getUsername(),
+                    loginRequest.getMatKhau()
+            );
+
+            return ResponseEntity.ok(Map.of("token", token));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body("Tài khoản hoặc mật khẩu không đúng!");
+        }
     }
 }
