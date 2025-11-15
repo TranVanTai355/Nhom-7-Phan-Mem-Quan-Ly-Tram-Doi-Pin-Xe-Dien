6813d01cbd65761a889e7fba81d678f1418b1f39
minhduc2207 <taminhduc22072005@gmail.com>
2025-11-16 05:35:31 +0700

    Update TaiXeRepository.java

--- src/main/java/com/nhom7/quanlytrampin/repository/TaiXeRepository.java
+++ src/main/java/com/nhom7/quanlytrampin/repository/TaiXeRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface TaiXeRepository extends JpaRepository<TaiXe, Long> {
 
     Optional<TaiXe> findByEmail(String email);
+    Optional<TaiXe> findByUsername(String username);
 }
