95461b95b8be8ce09d190d2fe6f521113e4c3437
minhduc2207 <taminhduc22072005@gmail.com>
2025-11-16 05:41:30 +0700

    Update TaiXe.java

--- src/main/java/com/nhom7/quanlytrampin/entity/TaiXe.java
+++ src/main/java/com/nhom7/quanlytrampin/entity/TaiXe.java
@@ -1,28 +1,42 @@
 package com.nhom7.quanlytrampin.entity;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class TaiXe { 
+@Table(name= "tai_xe")
+public class TaiXe implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long maTaiXe; 
+    private Long maTaiXe;
 
     @Column(nullable = false)
-    private String hoTen; 
+    private String hoTen;
 
     @Column(unique = true, nullable = false)
-    private String soDienThoai; 
+    private String soDienThoai;
 
     @Column(unique = true)
     private String email;
 
     @Column(nullable = false)
-    private String matKhau; 
+    private String matKhau;
+
+    @Column(nullable = false)
+    private String role; 
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
     @OneToMany(mappedBy = "taiXe")
     private List<PhuongTien> danhSachPhuongTien;
+
+    
 
     public Long getMaTaiXe() {
         return maTaiXe;
@@ -72,5 +86,51 @@ public class TaiXe {
         this.danhSachPhuongTien = danhSachPhuongTien;
     }
 
-    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role));
+    }
+
+    @Override
+    public String getPassword() {      
+        return this.matKhau;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; 
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; 
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; 
+    }
 }
