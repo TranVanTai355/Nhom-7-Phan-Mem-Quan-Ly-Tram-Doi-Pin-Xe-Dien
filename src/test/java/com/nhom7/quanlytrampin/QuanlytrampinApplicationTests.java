a34ebdd77c85f10aab1472c365b2ee36774db163
thanhnvd1101 <thanhnvd1101@ut.edu.vn>
2025-11-15 19:43:12 +0700

    Add files via upload

---
+++ src/test/java/com/nhom7/quanlytrampin/QuanlytrampinApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nhom7.quanlytrampin; // Phải đúng package
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlytrampinApplicationTests { // Phải đúng tên class
+
+    @Test
+    void contextLoads() {
+        // Hàm này kiểm tra xem ngữ cảnh ứng dụng Spring có tải thành công không
+    }
+
+}
