00224feaef1ef6cf87840384a8f908b9379e5d3e
minhduc2207 <taminhduc22072005@gmail.com>
2025-11-22 23:18:53 +0700

    Update JwtService.java

--- src/main/java/com/nhom7/quanlytrampin/service/JwtService.java
+++ src/main/java/com/nhom7/quanlytrampin/service/JwtService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
 
-    public static final String SECRET = "MATKHAULAKHONGBIET";
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
