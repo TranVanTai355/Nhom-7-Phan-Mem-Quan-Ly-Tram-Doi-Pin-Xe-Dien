b3f17ed60801fef995c17a308a8875a94d0c579b
minhduc2207 <taminhduc22072005@gmail.com>
2025-11-23 00:59:04 +0700

    Update AuthController.java

--- src/main/java/com/nhom7/quanlytrampin/controller/AuthController.java
+++ src/main/java/com/nhom7/quanlytrampin/controller/AuthController.java
@@ -6,7 +6,8 @@ import com.nhom7.quanlytrampin.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
+import java.util.Map; 
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -27,11 +28,13 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
         try {
+            
             String token = authService.loginUser(
                     loginRequest.getUsername(),
                     loginRequest.getMatKhau()
             );
 
+            
             return ResponseEntity.ok(Map.of("token", token));
 
         } catch (Exception e) {
